1e4087ab18f24220142f4e7f94fea51151d514e9
whitakerrestorations <inspirontechnologysolutions@gmail.com>
2025-07-30 13:53:00 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,15 @@
     };
     spinner();
     
+	setTimeout(function() {
+		if( $('#contact_formModal', document).hasClass('show') || $('#contactSmallModal', document).hasClass('show') ){
+			//console.log('already open');
+		} else {
+			//const myModal = bootstrap.Modal.getInstance($('#contactSmallModal')[0]);
+			//myModal.show();
+			$('.btn-contactSM_btn', document).trigger('click');
+		}
+    }, 5000); // 10000 milliseconds = 5 seconds
     
     // Initiate the wowjs
     new WOW().init();
@@ -34,19 +43,47 @@
 		var boxInitialTop = $('#logoBar').offset().top;
 		function sticky(){
 			if ($(window).scrollTop() > boxInitialTop) {
-			  $('#logoBar').css({position: 'fixed', top: '0px','z-index': '2'});
-			  $('#navbar').css({position: 'fixed', top: '110px','z-index': '2'});
+				$('#logoBar').addClass('position-sticky fixed-top');//.css({position: 'fixed', top: '0px','z-index': '2'});
+				$('#navbar').css({position: 'fixed', top: '110px','z-index': '2'});
 
-			  $('#logoBar img').css({'max-height': '1rem !important'}); 
+				$('#logoBar img').css({'max-height': '1rem !important'}); 
 			} else {
-			  $('#navbar').css({position: 'static'});
-				  $('#logoBar').css({position: 'static'});
+				$('#navbar').css({position: 'static'});
+				$('#logoBar').removeClass('position-sticky fixed-top');//.css({position: 'static'});
 			}
 		}
 		//if (window.matchMedia("(min-width: 992px)").matches){
 			$(window).scroll(sticky);
 			$(window).resize(sticky);
 		//}
-	});
+	}).on('click', '.btn-contactSM_btn', function () {
+		$(this).hide(); // Hide the button
+		$('body').css('overflow', 'auto');
+		//var myModal = new bootstrap.Modal(document.getElementById('chatModal'));
+		//myModal.show();
+    }).on('click', '.btn-contactFormModal', function(e){
+		var this_modal = $(this).parents('.modal');//.attr('id');
+		const myModal = bootstrap.Modal.getInstance(this_modal[0]); //new bootstrap.Modal( this_modal[0] );
+		myModal.hide();
+		
+	}).on('click', '#contact_form .btn-close, #contactSmallModal .btn-close', function(e){
+		$('.btn-contactSM_btn').show();
+	}).on('hidden.bs.modal', function (e) {
+		//console.log(e);
+		if(e.target.id == 'contact_formModal'){
+			$('.btn-contactSM_btn').show();
+		}
+    });
 
 })(jQuery);
+
+
+    // Get the query parameters from the URL
+    const params = new URLSearchParams(window.location.search);
+    const message = params.get('message'); // Get 'message' parameter
+
+    // Display it if it exists
+    if (message) {
+	const message ="We received your Message, Thank you.";    
+      document.getElementById('message').textContent = decodeURIComponent(message);
+    }
